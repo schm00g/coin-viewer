142d3ba50664ace1391c10bfac880441dbde22bc
schm00g <hk8igureu@relay.firefox.com>
2022-04-13 09:20:18 +0100

    chore: added nuxt-buefy

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    'nuxt-buefy',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
